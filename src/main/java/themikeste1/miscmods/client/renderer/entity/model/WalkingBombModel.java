9a21700328120c5c6d84b955059ec91939808357
Michael Hegerhorst <themikeste1@gmail.com>
2020-03-22 20:16:57 -0600

    WalkingBomb crown separation

--- src/main/java/themikeste1/miscmods/client/renderer/entity/model/WalkingBombModel.java
+++ src/main/java/themikeste1/miscmods/client/renderer/entity/model/WalkingBombModel.java
@@ -13,54 +13,77 @@ import themikeste1.miscmods.core.entities.WalkingBombEntity;
 @OnlyIn(Dist.CLIENT)
 public class WalkingBombModel<T extends WalkingBombEntity> extends SegmentedModel<T> {
     private final ModelRenderer face;
-    private final ModelRenderer topCrown;
-    private final ModelRenderer bottomCrown;
-    private final ModelRenderer leftCrown;
-    private final ModelRenderer rightCrown;
-    private final ModelRenderer backCrown;
+    private final ModelRenderer outerTopCrown;
+    private final ModelRenderer innerTopCrown;
+    private final ModelRenderer outerBottomCrown;
+    private final ModelRenderer innerBottomCrown;
+    private final ModelRenderer outerLeftCrown;
+    private final ModelRenderer innerLeftCrown;
+    private final ModelRenderer outerRightCrown;
+    private final ModelRenderer innerRightCrown;
+    private final ModelRenderer outerBackCrown;
+    private final ModelRenderer innerBackCrown;
+
 
     public WalkingBombModel() { this(0.0f); }
-
-
-
 
 
     public WalkingBombModel(float scale) {
 
         /*
          * TODO:
-         *  The textures do not work correctly when modeling the crown with the larger part of the body. I need to separate them.
+         *  Swap inner <-> outer
          */
         //Face
         //         ModelRenderer(Model, textureOffsetX, textureOffsetY)
         face = new ModelRenderer(this, 0, 0);
         face.setRotationPoint(0, 16, 0);
         face.addBox(-4f, -6f, -5f, 8.0F, 8.0F, 1.0F, scale);
+
         //Top Crown
-        topCrown = new ModelRenderer(this, 0, 9);
-        topCrown.setRotationPoint(0, 16, 0);
-        topCrown.addBox(-4f, -7f, -4f, 8.0F, 1.0F, 8.0F, scale);
-        topCrown.addBox(-3f, -8f, -3f, 6.0F, 1.0F, 6.0F, scale);
+        outerTopCrown = new ModelRenderer(this, 0, 9);
+        outerTopCrown.setRotationPoint(0, 16, 0);
+        outerTopCrown.addBox(-4f, -7f, -4f, 8.0F, 1.0F, 8.0F, scale);
+
+        innerTopCrown = new ModelRenderer(this, 0, 9);
+        innerTopCrown.setRotationPoint(0, 16, 0);
+        innerTopCrown.addBox(-3f, -8f, -3f, 6.0F, 1.0F, 6.0F, scale);
+
         //Bottom Crown
-        bottomCrown = new ModelRenderer(this, 0, 9);
-        bottomCrown.setRotationPoint(0, 16, 0);
-        bottomCrown.addBox(-4f, 2f, -4f, 8.0F, 1.0F, 8.0F, scale);
-        bottomCrown.addBox(-3f, 3f, -3f, 6.0F, 1.0F, 6.0F, scale);
+        outerBottomCrown = new ModelRenderer(this, 0, 9);
+        outerBottomCrown.setRotationPoint(0, 16, 0);
+        outerBottomCrown.addBox(-4f, 2f, -4f, 8.0F, 1.0F, 8.0F, scale);
+
+        innerBottomCrown = new ModelRenderer(this, 0, 9);
+        innerBottomCrown.setRotationPoint(0, 16, 0);
+        innerBottomCrown.addBox(-3f, 3f, -3f, 6.0F, 1.0F, 6.0F, scale);
+
         //Left Crown
-        leftCrown = new ModelRenderer(this, 0, 9);
-        leftCrown.setRotationPoint(0, 16, 0);
-        leftCrown.addBox(-5f, -6f, -4f, 1.0F, 8.0F, 8.0F, scale);
-        leftCrown.addBox(-6f, -5f, -3f, 1.0F, 6.0F, 6.0F, scale);
+        outerLeftCrown = new ModelRenderer(this, 0, 9);
+        outerLeftCrown.setRotationPoint(0, 16, 0);
+        outerLeftCrown.addBox(-5f, -6f, -4f, 1.0F, 8.0F, 8.0F, scale);
+
+        innerLeftCrown = new ModelRenderer(this, 0, 9);
+        innerLeftCrown.setRotationPoint(0, 16, 0);
+        innerLeftCrown.addBox(-6f, -5f, -3f, 1.0F, 6.0F, 6.0F, scale);
+
         //Right Crown
-        rightCrown = new ModelRenderer(this, 0, 9);
-        rightCrown.setRotationPoint(0, 16, 0);
-        rightCrown.addBox(5f, -6f, -4f, -1.0F, 8.0F, 8.0F, scale);
-        rightCrown.addBox(6f, -5f, -3f, -1.0F, 6.0F, 6.0F, scale);
+        outerRightCrown = new ModelRenderer(this, 0, 9);
+        outerRightCrown.setRotationPoint(0, 16, 0);
+        outerRightCrown.addBox(5f, -6f, -4f, -1.0F, 8.0F, 8.0F, scale);
+
+        innerRightCrown = new ModelRenderer(this, 0, 9);
+        innerRightCrown.setRotationPoint(0, 16, 0);
+        innerRightCrown.addBox(6f, -5f, -3f, -1.0F, 6.0F, 6.0F, scale);
+
         //Back Crown
-        backCrown = new ModelRenderer(this, 0, 9);
-        backCrown.setRotationPoint(0, 16, 0);
-        backCrown.addBox(-4f, -6f, 4f, 8.0F, 8.0F, 1.0F, scale);
-        backCrown.addBox(-3f, -5f, 5f, 6.0F, 6.0F, 1.0F, scale);
+        outerBackCrown = new ModelRenderer(this, 0, 9);
+        outerBackCrown.setRotationPoint(0, 16, 0);
+        outerBackCrown.addBox(-4f, -6f, 4f, 8.0F, 8.0F, 1.0F, scale);
+
+        innerBackCrown = new ModelRenderer(this, 0, 9);
+        innerBackCrown.setRotationPoint(0, 16, 0);
+        innerBackCrown.addBox(-3f, -5f, 5f, 6.0F, 6.0F, 1.0F, scale);
 
     }
 
@@ -78,6 +101,6 @@ public class WalkingBombModel<T extends WalkingBombEntity> extends SegmentedMode
 
     @Override
     public Iterable<ModelRenderer> getParts() {
-        return ImmutableList.of(face, topCrown, bottomCrown, leftCrown, rightCrown, backCrown);
+        return ImmutableList.of(face, innerTopCrown/*, outerBottomCrown, outerLeftCrown, outerRightCrown, outerBackCrown*/);
     }
 }
